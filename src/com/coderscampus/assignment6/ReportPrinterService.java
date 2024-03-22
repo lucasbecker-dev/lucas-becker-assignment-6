6f38f25c3639c01939e03a4cb893b79d83d62454
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-22 16:45:43 -0400

    printReport takes ModelType enum instead of String

--- src/com/coderscampus/assignment6/ReportPrinterService.java
+++ src/com/coderscampus/assignment6/ReportPrinterService.java
@@ -4,17 +4,17 @@ import java.time.YearMonth;
 import java.util.List;
 
 public class ReportPrinterService {
-    public static void printReport (String model, List<String> reportData, YearMonth bestMonth, YearMonth worstMonth) {
-        if (model == null || reportData == null || bestMonth == null || worstMonth == null) {
+    public static void printReport (ModelType modelType, List<String> reportData, YearMonth bestMonth, YearMonth worstMonth) {
+        if (modelType == null || reportData == null || bestMonth == null || worstMonth == null) {
             System.err.println("ReportPrinterService.printReport cannot accept null data.");
             return;
         }
-        System.out.println(model + " Yearly Sales Report");
+        System.out.println(modelType + " Yearly Sales Report");
         System.out.println("---------------------------");
         reportData.forEach(System.out::println);
         System.out.println();
-        System.out.println("The best month for " + model + " was: " + bestMonth);
-        System.out.println("The worst month for " + model + " was: " + worstMonth);
+        System.out.println("The best month for " + modelType + " was: " + bestMonth);
+        System.out.println("The worst month for " + modelType + " was: " + worstMonth);
         System.out.println();
     }
 }
