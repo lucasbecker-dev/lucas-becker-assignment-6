3627dda090638053db53b06d9692e69c8632f963
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-22 16:29:13 -0400

    initial application testing (needs work!)

--- src/com/coderscampus/assignment6/Assignment6Application.java
+++ src/com/coderscampus/assignment6/Assignment6Application.java
@@ -1,4 +1,34 @@
 package com.coderscampus.assignment6;
 
+import java.time.YearMonth;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class Assignment6Application {
+    public static void main(String[] args) {
+        List<SalesData> model3 = FileService.readFile("src/com/coderscampus/assignment6/model3.csv");
+        List<SalesData> modelS = FileService.readFile("src/com/coderscampus/assignment6/modelS.csv");
+        List<SalesData> modelX = FileService.readFile("src/com/coderscampus/assignment6/modelX.csv");
+
+        System.out.println(SalesDataService.findSalesDataBestMonth(model3));
+        System.out.println(SalesDataService.findSalesDataWorstMonth(model3));
+
+
+        Optional<SalesData> model3WorstMonthOpt = modelX.stream().min(Comparator.comparingInt(SalesDataService::getSalesAsInteger));
+        Optional<SalesData> modelSWorstMonthOpt = modelX.stream().min(Comparator.comparingInt(SalesDataService::getSalesAsInteger));
+        Optional<SalesData> modelXWorstMonthOpt = modelX.stream().min(Comparator.comparingInt(SalesDataService::getSalesAsInteger));
+
+
+        Stream<YearMonth> yearMonthModel3 = model3.stream().map(SalesDataService::getDateAsYearMonth);
+        Stream<YearMonth> yearMonthModelS = modelS.stream().map(SalesDataService::getDateAsYearMonth);
+        Stream<YearMonth> yearMonthModelX = modelX.stream().map(SalesDataService::getDateAsYearMonth);
+//
+//        yearMonthModel3.forEach(System.out::println);
+//
+//        System.out.println("Model 3 Yearly Sales Report");
+//        System.out.println("---------------------------");
+
+    }
 }
