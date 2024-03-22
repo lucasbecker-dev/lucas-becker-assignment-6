18c1c7a9c73585fe9a374ede075409e5599cd24a
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-22 16:36:34 -0400

    initial printReport() draft

--- src/com/coderscampus/assignment6/ReportPrinterService.java
+++ src/com/coderscampus/assignment6/ReportPrinterService.java
@@ -1,4 +1,20 @@
 package com.coderscampus.assignment6;
 
+import java.time.YearMonth;
+import java.util.List;
+
 public class ReportPrinterService {
+    public static void printReport (String model, List<String> reportData, YearMonth bestMonth, YearMonth worstMonth) {
+        if (model == null || reportData == null || bestMonth == null || worstMonth == null) {
+            System.err.println("ReportPrinterService.printReport cannot accept null data.");
+            return;
+        }
+        System.out.println(model + " Yearly Sales Report");
+        System.out.println("---------------------------");
+        reportData.forEach(System.out::println);
+        System.out.println();
+        System.out.println("The best month for " + model + " was: " + bestMonth);
+        System.out.println("The worst month for " + model + " was: " + worstMonth);
+        System.out.println();
+    }
 }
