7380fea6694998f1279528b6f4622c51452b1414
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-22 16:50:06 -0400

    printReport overload which uses SalesData list directly

--- src/com/coderscampus/assignment6/ReportPrinterService.java
+++ src/com/coderscampus/assignment6/ReportPrinterService.java
@@ -17,4 +17,21 @@ public class ReportPrinterService {
         System.out.println("The worst month for " + modelType + " was: " + worstMonth);
         System.out.println();
     }
+
+    public static void printReport (ModelType modelType, List<SalesData> reportData) {
+        if (modelType == null || reportData == null) {
+            System.err.println("ReportPrinterService.printReport cannot accept null data.");
+            return;
+        }
+        YearMonth bestMonth = SalesDataService.getDateAsYearMonth(SalesDataService.findSalesDataBestMonth(reportData));
+        YearMonth worstMonth = SalesDataService.getDateAsYearMonth(SalesDataService.findSalesDataWorstMonth(reportData));
+
+        System.out.println(modelType + " Yearly Sales Report");
+        System.out.println("---------------------------");
+        reportData.forEach(System.out::println);
+        System.out.println();
+        System.out.println("The best month for " + modelType + " was: " + bestMonth);
+        System.out.println("The worst month for " + modelType + " was: " + worstMonth);
+        System.out.println();
+    }
 }
