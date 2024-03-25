24a3cf4d8250a2312a15d7f1eb000e330085e73d
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-25 14:37:25 -0400

    parseYearMonthToString no longer has Optional parameter

--- src/com/coderscampus/assignment6/SalesDataService.java
+++ src/com/coderscampus/assignment6/SalesDataService.java
@@ -3,15 +3,26 @@ package com.coderscampus.assignment6;
 import java.time.DateTimeException;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SalesDataService {
 
-    public static Optional<String> parseYearMonthToString(SalesData salesData, Optional<String> optionalFormatPattern) {
-        final String DEFAULT_FORMATTER_PATTERN = "yyyy-MM";
+    public static Optional<String> parseYearMonthToString(SalesData salesData) {
+        final String DEFAULT_FORMAT_PATTERN = "yyyy-MM";
         try {
-            String formatPattern = optionalFormatPattern.orElse(DEFAULT_FORMATTER_PATTERN);
+            return Optional.of(salesData.getDate().format(DateTimeFormatter.ofPattern(DEFAULT_FORMAT_PATTERN)));
+        } catch (NullPointerException | DateTimeException | IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<String> parseYearMonthToString(SalesData salesData, String formatPattern) {
+        try {
             return Optional.of(salesData.getDate().format(DateTimeFormatter.ofPattern(formatPattern)));
         } catch (NullPointerException | DateTimeException | IllegalArgumentException e) {
             System.err.println(e.getMessage());
