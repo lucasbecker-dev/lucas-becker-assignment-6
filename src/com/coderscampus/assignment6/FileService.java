ce31a65a2eec616ed8db3b83dc46a284f0ef5165
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-25 10:25:19 -0400

    parsing now done at file read time instead of when being read from pojo

--- src/com/coderscampus/assignment6/FileService.java
+++ src/com/coderscampus/assignment6/FileService.java
@@ -1,12 +1,15 @@
 package com.coderscampus.assignment6;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Stream;
+import java.time.YearMonth;
 
 public class FileService {
     public static List<SalesData> readFile(String filepath){
@@ -14,11 +17,21 @@ public class FileService {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
             Stream<String> lines = reader.lines().skip(1);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-yy", Locale.US);
             lines.forEach(line -> {
                 String[] splitLine = line.split(",");
-                salesData.add(new SalesData(splitLine[0], splitLine[1]));
+                YearMonth yearMonth = YearMonth.parse(splitLine[0], formatter);
+                Integer sales = Integer.parseInt(splitLine[1]);
+                salesData.add(new SalesData(yearMonth, sales));
             });
         } catch (IOException e) {
+            System.err.println("Unable to read file: " + filepath);
+            System.err.println(e.getMessage());
+        } catch (DateTimeParseException e) {
+            System.err.println("Year and month in csv file: " + filepath + " could not be parsed.");
+            System.err.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Number of sales in: " + filepath + " could not be parsed.");
             System.err.println(e.getMessage());
         }
 
